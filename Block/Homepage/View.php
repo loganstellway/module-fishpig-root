1f730e3413ed2f25f8b945d7fbfd97fd3734f400
vagrant <vagrant@localhost.localdomain>
2017-12-20 01:16:39 +0000

    Updates homepage layout to allow appending child elements

---
+++ Block/Homepage/View.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LoganStellway\FishPigRoot\Block\Homepage;
+
+/**
+ * Homepage View
+ */
+class View extends \Magento\Framework\View\Element\Template {
+    /**
+     * Dependency Injection
+     */
+    public function __construct(
+        \FishPig\WordPress\Model\App $app,
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_app = $app;
+    }
+
+    /**
+     * To HTML
+     */
+    protected function _toHtml()
+    {
+        if ($this->_app->getHomepagePageId()) {
+            return $this->getChildBlock('lsfishpigroot.homepage.view')->setPost(
+                \Magento\Framework\App\ObjectManager::getInstance()->create('FishPig\WordPress\Model\Post')->load(
+                    $this->_app->getHomepagePageId()
+                )
+            )->toHtml();
+        }
+        return '';
+    }
+}
